c59b06f855965cace098bb84e58cb5d3c3b32c7e
shinigami <edgareduardoespindola@gmail.com>
2017-10-24 19:32:43 -0500

    Cambiar tipo de columna en servidor

--- db/migrate/20171012215336_add_kilogramos_netos_to_line_item_salida.rb
+++ db/migrate/20171012215336_add_kilogramos_netos_to_line_item_salida.rb
@@ -1,5 +1,5 @@
 class AddKilogramosNetosToLineItemSalida < ActiveRecord::Migration[5.0]
   def change
-    add_column :line_item_salidas, :total_kilogramos_netos, :string, default: '0.0'
+    add_column :line_item_salidas, :total_kilogramos_netos, :integer, default: 0
   end
 end
