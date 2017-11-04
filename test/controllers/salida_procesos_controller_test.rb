cbaf8de3ce3dcc322fe0a9123207f131f4499b0f
shinigami <edgareduardoespindola@gmail.com>
2017-11-04 13:12:39 -0600

    Se agrega el HTML en las pruebas

--- test/controllers/salida_procesos_controller_test.rb
+++ test/controllers/salida_procesos_controller_test.rb
@@ -8,6 +8,11 @@ class SalidaProcesosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get salida_procesos_url
     assert_response :success
+    
+    assert_select ".row .col-sm-12 h1.title-md", 1
+    assert_select "h1.text-uppercase", I18n.t('salida_procesos.index.title')
+    assert_select ".table-responsive table.table", 1
+    assert_select "table tbody tr", minimum: 2
   end
 
   test "should create salida_proceso" do    
@@ -41,6 +46,12 @@ class SalidaProcesosControllerTest < ActionDispatch::IntegrationTest
   test "should show salida_proceso" do
     get salida_proceso_url(@salida_proceso)
     assert_response :success
+    
+    assert_select "h1.title-md", I18n.t('salida_procesos.show.title')
+    assert_select "ul.nav.nav-tabs.nav-tabs-shop li a", 'Información Salida'
+    assert_select "div.tab-content div.tab-pane#specifications", 1
+    assert_select "div.btn-group a", 1
+    assert_select "table.table.table-striped", minimum: 1
   end
 
 end
